2b4b36b4d0356d7e1ba9bdadfecb8d666ecf8c32
ABeloeil <abeloeil@u-pro.fr>
2017-06-30 10:17:26 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const colors = require('colors');
+const path = require('path');
+const readLine = require('readline');
+
+console.log('------------------------'.green);
+console.log('Building parameters file'.green.bold);
+console.log('------------------------'.green);
